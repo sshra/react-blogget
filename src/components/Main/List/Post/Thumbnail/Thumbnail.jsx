8a74a1da061297748163be37067b9f9f8f66c2c1
Yuriy Korol as shra <sshra@mail.ru>
2022-12-17 22:56:26 +0500

    Url verification for thumbnail image

--- src/components/Main/List/Post/Thumbnail/Thumbnail.jsx
+++ src/components/Main/List/Post/Thumbnail/Thumbnail.jsx
@@ -1,12 +1,12 @@
 import style from './Thumbnail.module.css';
 import PropTypes from 'prop-types';
 import notphoto from './img/notphoto.jpg';
+import isUrl from 'is-url';
 
 export const Thumbnail = ({ src, altTitle }) =>
   <img
     className={style.img}
-    src={src &&
-      !(['self', 'spoiler', 'default']).includes(src) ? src : notphoto}
+    src={src && isUrl(src) ? src : notphoto}
     alt={altTitle} />;
 
 Thumbnail.propTypes = {
