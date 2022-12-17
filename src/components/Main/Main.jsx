02ad0ea1e4f7a50ee79877679719eebc5415ea81
Yuriy Korol as shra <sshra@mail.ru>
2022-12-17 22:04:09 +0500

    Fixes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import Layout from '../Layout';
 import style from './Main.module.css';
 import Tabs from './Tabs';
 import List from './List';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Modal from '../../UI/Modal';
 import FullPost from './List/Post/FullPost';
 import FrontPage from './FrontPage';
@@ -14,12 +14,13 @@ export const Main = () =>
       <Tabs/>
       <Routes>
         <Route path='/' element={<FrontPage/>} />
-        <Route path='/auth' element={<FrontPage/>} />
+        <Route path='/auth' element={<Navigate to='/'/>} />
         <Route path='/category/:page' element={
           <List pageSize={5} autoloadDepth={2} />}>
           <Route path='post/:id' element={ <Modal><FullPost /></Modal>} />
         </Route>
-        <Route path="*" element={<Page404/>}/>
+        <Route path="/404" element={<Page404/>}/>
+        <Route path="*" element={<Navigate to='/404'/>}/>
       </Routes>
     </Layout>
   </main>;
